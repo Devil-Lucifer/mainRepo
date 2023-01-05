f4516f99a77ba899a04c7e4a05f798e5a99dc26e
HarishRaja0211 <hayalishi0211@gmail.com>
2023-01-05 11:13:44 +0530

    added function to string in list

--- hi.py
+++ hi.py
@@ -2,3 +2,14 @@ print('hi')
 
 #fork pracctice
 print('just added an print statement \U0001F600')
+
+print()
+
+#function to  add string into list...
+def new_list(sentence):
+    
+    newlist=list(sentence)
+    newlist += [sentence]
+    
+    return newlist
+
