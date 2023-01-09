a29995131ab0deac9784a1180b261bbfe3e620ca
Devil-Lucifer <harshchauhan198@gmail.com>
2023-01-09 14:42:07 +0530

    Made some improvements

--- User_calculator.py
+++ User_calculator.py
@@ -1,7 +1,12 @@
+while(True):
+    try:
+        print("Enter the Two no to be added ")
+        varA=int(input("Enter A no:"))
+        varB= int(input("Enter 2nd no :"))
 
-print("Enter the Two no to be added ")
-varA=int(input("Enter A no:"))
-varB= int(input("Enter 2nd no :"))
+        varC=varA+varB
 
-varC=varA+varB
-print("the sum of two no  :",varC)
+        print("the sum of two no  :",varC)
+        break
+    except :
+        print("enter a int value for it to work  ")
